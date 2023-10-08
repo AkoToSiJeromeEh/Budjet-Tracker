8e14a3e976058a0969c9d0b22359d52f0189bc30
AkoToSiJeromeEh <jevi.gaspar.au@phinmaed.com>
2023-10-08 21:51:30 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+export default {
+  content: [
+    "./index.html","./pages/**/*.{js,ts,jsx,tsx,html}",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+
+      colors: {
+
+         primary : '#F9F9F9',
+         secondary : '#BEAEE2',
+         accent : '#CDF0EA'
+      }
+    },
+  },
+  plugins: [],
+}
